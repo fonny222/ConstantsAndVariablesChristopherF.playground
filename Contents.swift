bce799b57de4addf64a46e66bf6957686366860c
fonny222 <cfontana@e112-m-18.mchenry.edu>
2016-09-06 16:43:21 -0500

    ConstantsAndVariables
    9/6/16 experimenting with constants and variables

---
+++ Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Listing Variables first
+
+var variableInt = 12
+
+var variableFloat = 1.2
+
+var variableDouble = 1.2532
+
+
+// listing same data types but as constants
+
+let constantInt = 24
+
+let constantFloat = 2.2
+
+let constantDouble = 2.2532
+
+// Listing explicit constants
+
+let explicitString:String = "Helloooo There!"
+
+let explicitFloat:Float = 1.2
+
+let explicitDouble:Double = 1.1234523
+
+
+// Created a constant
+
+let number1 = 1
+
+// changed value on the next line
+
+number1 = 2
+
+//Created a constant with the explicit type of Float and value of 4
+// the program ran as normal displaying the 4 on the side and not 
+// displaying an error message
+
+let number2:Float = 4
+
+
+
